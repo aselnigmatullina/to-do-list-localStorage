544bd2eb4ac5d8ebe6840dffe635b074a5ad8cd2
aselnigmatullina <nogmatullina.asel2016@yandex.ru>
2019-11-18 21:16:34 +0300

    создала to-do list

---
+++ app.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('form')
+const ul = document.querySelector('ul')
+const button = document.querySelector('button')
+const input = document.getElementById('item')
+let itemsArray = localStorage.getItem('items') ?
+JSON.parse(localStorage.getItem('items')) : []
+
+
+localStorage.setItem('items', JSON.stringify(itemsArray))
+const data = JSON.parse(localStorage.getItem('items'))
+
+
+const linkMaker = text => {
+    const li = document.createElement('li')
+    li.textContent = text
+    ul.appendChild(li)
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    itemsArray.push(input.value)
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+    linkMaker(input.value)
+    input.value = ''
+})
+
+data.forEach(item => linkMaker(item))
+
+
+button.addEventListener('click', () => {
+    localStorage.clear()
+    while(ul.firstChild){
+    ul.removeChild(ul.firstChild)
+    }
+})
